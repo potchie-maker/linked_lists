daac55782ce8c8e32257c46ed498e2fdb2834de3
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2025-05-07 15:50:33 -0400

    Edit README and add files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # linked_lists
+
+This project is an exercise of implementing linked lists inside of Ruby.
---
+++ main.rb
@@ -0,0 +1,12 @@
+require_relative 'lib/classes'
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+
+puts list
